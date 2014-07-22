934687ba63b761408a232d60f1b0dd8b975424fb
asbudik <asbudik@live.com>
2014-07-22 16:07:47 -0700

    from dawn lab to finish tonight

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-var express = require("express");
+var express = require("express"),
+  request = require("request");
 
 var app = express();
 
 app.get('/', function(req, res){
   res.render('index.ejs');
 });
+
+app.get('/search', function(req, res){
+  var query = req.query.searchTerm;
+  var url = "http://www.omdbapi.com/?s=" + query;
+  request(url, function(error, response, body) {
+    if (!error) {
+      var data = JSON.parse(body);
+      res.render("results.ejs", {movieList: data.Search || []});
+    } else {
+      res.send("error occured");
+    }
+  })
+});
+
+
 
 app.listen(3000);
